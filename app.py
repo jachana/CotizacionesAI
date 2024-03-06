2d796760051021a4f5b30dfdca9ba1959b309c82
jachana <jachana@users.noreply.github.com>
2024-03-06 23:19:49 +0000

    a

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ if prompt := st.chat_input("Que productos quiere cotizar"):
         st.markdown(response)
         # wait for the assistant to respond
         if st.button("Reset", type="primary"):
+            del st.session_state.messages
             st.switch_page("app.py")
 
     st.session_state.messages.append({"role": "assistant", "content": response})
