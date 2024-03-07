fc4dee883b409a58393ee04b243cff8b231151f3
julio chana <julio.chana@tobii.com>
2024-03-07 09:50:56 +0100

    test

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ if prompt := st.chat_input("Que productos quiere cotizar"):
         with st.spinner(text="cotizando..."):
             markdown, detail, total = main.find_alternatives(prompt)
         st.markdown(markdown)
-        add_quote_button(detail,total,message_index+1)
+        add_quote_button(detail,total,message_index+2)
         # wait for the assistant to respond
 
 
